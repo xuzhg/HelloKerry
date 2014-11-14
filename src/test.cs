94a2d9edafe5e7459cd9f13a5383142982eabef5
unknown <saxu@sam-xu-odata.fareast.corp.microsoft.com>
2014-11-14 17:14:35 +0800

    test

---
+++ src/test.cs
@@ -0,0 +1,22 @@
+
+                    /*
+                    EntityTypeConfiguration targetEntity = _configurations.FirstOrDefault(e => e.ClrType == navProp.RelatedClrType) as EntityTypeConfiguration;
+                    ValidateReferentialConstraint(targetEntity, navProp.ReferentialConstraint.PrincipalProperties);
+
+                    info.DependentProperties =
+                        navProp.ReferentialConstraint.PrincipalProperties.Select(property => _properties[property])
+                        .OfType<IEdmStructuralProperty>().ToList();
+
+                    //IList<NavigationPropertyConfiguration> parterNavigationProperties =
+                    //    targetEntity.NavigationProperties.Where(n => n.RelatedClrType == config.ClrType).ToList();
+                    EdmNavigationPropertyInfo parterInfo = new EdmNavigationPropertyInfo
+                    {
+                        Target = entityType,
+                        //Name = parterNavigationProperties.Count == 1 ? parterNavigationProperties[0].Name : null,
+                        DependentProperties =
+                            navProp.ReferentialConstraint.DependentProperties.Select(property => _properties[property])
+                                .OfType<IEdmStructuralProperty>()
+                                .ToList()
+                    };
+
+                    entityType.AddUnidirectionalNavigation(info, parterInfo);*/
